316800d876329b03b4bfc07e268ea93adfd50ed3
Alexandr Solopov <>
2022-12-19 20:52:06 +0300

    Task 09

---
+++ Task09/Program.cs
@@ -0,0 +1,45 @@
+﻿/*9. Напишите программу, которая
+1. выводит случайное число из отрезка [10, 99]
+2. показывает нибольшую цифру числа.
+
+78 -> 8
+12 -> 2
+85 -> 8*/
+
+/*int number = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрека 10 - 99 => {number}");
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;*/
+
+/*if (firstDigit > secondDigit)
+{
+    Console.WriteLine($"Наибольшая цифра числа => {firstDigit}");
+}
+else
+{
+    Console.WriteLine($"Наибольшая цифра числа => {secondDigit}");
+}*/
+
+
+/*int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+Console.WriteLine($"Наибольшая цифра числа => {result}");*/
+
+
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа => {maxDigit}");
+
+int maxDigit1 = MaxDigit(45);
+Console.WriteLine($"Наибольшая цифра числа => {maxDigit1}");
+
+int maxDigit2 = MaxDigit(78);
+Console.WriteLine($"Наибольшая цифра числа => {maxDigit2}");
+
+int MaxDigit(int num) // num = number
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    //int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}

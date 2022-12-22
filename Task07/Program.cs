b1427a5853669584a867053ce690aceb56ce297f
Alexandr Solopov <>
2022-12-23 00:03:36 +0300

    Task07

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -5,5 +5,9 @@
 782 -> 2
 918 -> 8*/
 
-
-
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number <= 1000)
+    Console.WriteLine($"Последняя цифра в числе {number} равна {number % 10}");
+else
+    Console.WriteLine("Вы ввели не трех значное число");

5fb45f2bf523dd76674123f0d26079c4a5bfeb1a
Alexandr Solopov <>
2022-12-22 22:25:27 +0300

    Task20

---
+++ Task20/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 22: Напишите программу, которая
+принимает на вход число (N) и выдаёт таблицу
+квадратов чисел от 1 до N.*/
+
+
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number > 0)
+{
+    TableSquare(number);
+}
+else System.Console.WriteLine("Не натуральное число");
+
+void TableSquare(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count,5} {Math.Pow(count, 2),5}");
+        count++;
+    }
+}

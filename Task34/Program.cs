b76007fcb65df1dd8c45a27b8ed7c5559e3d8256
Alexandr Solopov <>
2023-01-19 00:13:32 +0300

    Task34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -4,3 +4,27 @@
 // чисел в массиве.
 
 // [345, 897, 568, 234] -> 2
+
+void FillAray (int [] arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().Next (100,999);
+        Console.Write(arr [i] + " "); 
+    }
+}
+
+int[] array = new int[8];
+FillAray(array);
+int length = array.Length;
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0) 
+    count++;
+}
+Console.WriteLine();
+Console.WriteLine(" ");
+Console.WriteLine("Количество четных значений: " + count);

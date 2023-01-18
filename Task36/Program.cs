e4b0bef20872a1c42d579491d6276b174de0b98b
Alexandr Solopov <>
2023-01-19 00:13:47 +0300

    Task36

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -5,3 +5,35 @@
 // [3, 7, 23, 12] -> 19
 
 // [-4, -6, 89, 6] -> 0
+
+int[] CreateArrayRndInd (int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i == 0) Console.Write("[");
+        if(i < array.Length - 1) Console.Write(array[i] + ", ");
+        else Console.Write(array[i] + "]");
+    }
+}
+
+int[] array = CreateArrayRndInd(4, 1, 9);
+PrintArray(array);
+
+int sum = 0;
+for (int s = 0; s < array.Length; s+=2)
+{
+    sum = sum + array[s];
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма нечетных элементов массива -> {sum}");

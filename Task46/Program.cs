df0c0c7bd4244e14a7f7977bd226778be5c3a6ef
Alexandr Solopov <>
2023-01-27 00:11:43 +0300

    Task46

--- Task46/Program.cs
+++ Task46/Program.cs
@@ -14,7 +14,7 @@ int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 
     for (int i = 0; i < matrix.GetLength(0); i++) // 2
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++) 
         {
             matrix[i, j] = rnd.Next(min, max + 1);  // 2 - 3
         }
@@ -25,17 +25,16 @@ int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 
     void PrintMatrix(int[,] matrix)
     {
-        for (int i = 0; i < matrix.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0) i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            Console.Write("[");
-            for (int j = 0; j < matrix.GetLength(1); j++)
-            {
-                if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5},");
-                else Console.Write($"{matrix[i, j],5},");
-            }
-            Console.WriteLine("]");
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 5},");
+            else Console.Write($"{matrix[i, j],5},");
         }
+        Console.WriteLine("]");
     }
-    
+
     int[,] array2D = CreateMatrixRndInt(3, 4, -99, 99);
     PrintMatrix(array2D);
